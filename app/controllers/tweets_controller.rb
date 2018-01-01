2e6c60ff777dff210acce8673cfca42970c457e5
kobayashikenji <kenjilamancha@gmail.com>
2018-01-01 20:49:53 +0900

    2日目rails1完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,54 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @tweets = Tweet.all.order("id DESC").page(params[:page]).per(5)
+#    @tweets = Tweet.all.order("id DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("id DESC")
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id:current_user.id)
     #binding.pry
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def show
+    @tweet  = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
   end
 
   private
   def tweet_params
-    params.permit(:name, :image, :text)
+    params.permit(:image, :text)
     #binding.pry
   end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+    # indexアクションを強制的に実行する
+  end
+
+
 #メソッドの返り値はそのメソッドの中で最後に実行されている式の値
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tweets#index'
-  get  'tweets'      => 'tweets#index'           #ツイート一覧画面
-   #URLのパス        →  コントローラ#アクション
-  get  'tweets/new'  => 'tweets#new'    #ツイート投稿画面
-  post 'tweets'      => 'tweets#create' #ツイート投稿機能
+  root       'tweets#index'
+  resources  :tweets do
+    resources :comments, only: [:create]
+  end
+  resources  :users, only: [:show]
+end
+  # get     'tweets'      => 'tweets#index'           #ツイート一覧画面
+  #  #URLのパス        →  コントローラ#アクション
+  # get     'tweets/new'      => 'tweets#new'    #ツイート投稿画面
+  # post    'tweets'          => 'tweets#create' #ツイート投稿機能
+  # delete  'tweets/:id'      => 'tweets#destroy'
+  # patch   'tweets/:id'      => 'tweets#update'
+  # get     'tweets/:id/edit' => 'tweets#edit'
+  # get     'users/:id'       => 'users#show'    #MYPAGEへのルーティング
+  # get     'tweets/:id'      => 'tweets#show'   #ツイート詳細画面
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -61,4 +71,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
